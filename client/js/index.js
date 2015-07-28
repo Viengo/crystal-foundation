23d2b13bee3af7b81c43cca27fe4cc8c0b8bba6a
Viengo <viengooleg@gmail.com>
2015-07-28 19:28:30 +0400

    scroll-open js

--- client/js/index.js
+++ client/js/index.js
@@ -1,6 +1,5 @@
 
- //Slider Settings
-
+ //foundation settings
 
  $(document).on('ready', function () {
      $(document).foundation({
@@ -11,12 +10,31 @@
  });
 
 
- $(window).on("scroll", function () {
-     if ($(this).scrollTop() > 869) { //Any number you want
+//scroll-open effect
+ $(window).on("scroll touchmove", function () {
+     if ($('.contain-to-grid').toggleClass('fixed', $(document).scrollTop() > 869)) {
          $(".scroll-open").show()
      }
      else {
          $(".scroll-open").hide()
      }
  });
+
+
+
+
+// $(window).on("scroll", function () {
+//     if ($(this).scrollTop() > 869) { //Any number you want
+//         $(".scroll-open").show()
+//     }
+//     else {
+//         $(".scroll-open").hide()
+//     }
+// });
+
+ // $(window).on("scroll touchmove", function () {
+ //     $('.contain-to-grid').toggleClass('fixed', $(document).scrollTop() > 869);
+ //     $(".scroll-open").show()
+ //
+ // });
 
